f357a2aec89b5806b7d49f26f8a9308d4be0dfbd
Shaheryar Khan <emailshaheryar@gmail.com>
2024-01-21 19:01:34 +0500

    approval status change apis

--- SCMS/SCMS/Controllers/Approval/ApprovalController.cs
+++ SCMS/SCMS/Controllers/Approval/ApprovalController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using SCMS.Controllers.Helper;
 using SCMS.DTO;
 using SCMS.Models;
 
@@ -24,16 +25,73 @@ namespace SCMS.Controllers.Setup
             return result;
         }
 
-        #endregion Account Approval
 
-        #region Product Approval
+        [HttpPost("accountapproval")]
+        public SaveResponse AccountApproval(int id, int approval)
+        {
+            AccountUser _account = _context.AccountUsers.FirstOrDefault(account => account.Id == id);
+            if (_account != null)
+            {
+                _account.Approve = approval;
+                _context.AccountUsers.Update(_account);
+                _context.SaveChanges();
 
-        [HttpGet("getproductapproval")]
+                return new SaveResponse
+                {
+                    StatusCode = "000",
+                    Message = "record updated successfully",
+                    Session = clsSession.GetLogin(HttpContext.Session),
+                };
+            }
+
+            return new SaveResponse
+            {
+                StatusCode = "001",
+                Message = "record not found",
+                Session = clsSession.GetLogin(HttpContext.Session),
+            };
+
+        }
+
+
+
+            #endregion Account Approval
+
+            #region Product Approval
+
+            [HttpGet("getproductapproval")]
         public List<ProductApproval> GetProductApproval()
         {
             var result = (from o in _context.ProductApprovals select o).ToList();
 
             return result;
+        }
+
+        [HttpPost("productapproval")]
+        public SaveResponse ProductApproval(int id, int approval)
+        {
+            Product _product = _context.Products.FirstOrDefault(prodcut => prodcut.Id == id);
+            if (_product != null)
+            {
+                _product.Approve = approval;
+                _context.Products.Update(_product);
+                _context.SaveChanges();
+
+                return new SaveResponse
+                {
+                    StatusCode = "000",
+                    Message = "record updated successfully",
+                    Session = clsSession.GetLogin(HttpContext.Session),
+                };
+            }
+
+            return new SaveResponse
+            {
+                StatusCode = "001",
+                Message = "record not found",
+                Session = clsSession.GetLogin(HttpContext.Session),
+            };
+
         }
 
         #endregion Product Approval
